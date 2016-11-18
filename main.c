dd1e6799723865de0d7e40451c27ad42e3b00a79
Kevin Zhang <kevinzhang3702@gmail.com>
2016-11-18 11:23:53 -0500

    made things worse

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <string.h>
+#include <unistd.h>
 
 
 int main(){
@@ -9,6 +9,18 @@ int main(){
   printf("What would you like to do?: ");
   fgets(s,sizeof(s),stdin);
 
+  char * cmd[100];
+  char * currentString = s;
+  int i = 0;
+
+  while( currentString ){
+    cmd[i] = strsep(&currentString," ");
+    i++;
+  }
+  cmd[i] = 0;
+
+  execvp(cmd[0], cmd);
+  
   return 0;
 }
 
