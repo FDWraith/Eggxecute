73579a1a16ac1fcf076beac944cf85340c571e41
Kevin Zhang <kevinzhang3702@gmail.com>
2016-11-18 11:43:13 -0500

    found a solution, but not sure how to do it

--- main.c
+++ main.c
@@ -9,17 +9,31 @@ int main(){
   printf("What would you like to do?: ");
   fgets(s,sizeof(s),stdin);
 
+  
+
   char * cmd[100];
   char * currentString = s;
   int i = 0;
-
+  
   while( currentString ){
     cmd[i] = strsep(&currentString," ");
+    printf("Added %s\n", cmd[i]);
     i++;
   }
-  cmd[i] = 0;
 
+  //Get rid of newLine char
+  
+
+  
+  cmd[i-1] = strcpy( cmd[i-1], )
+  
+  
+  cmd[i] = 0;
+  printf("cmd[0]: %s \n", cmd[0]);
+  printf("cmd[1]: %s \n", cmd[1]);
+  
   execvp(cmd[0], cmd);
+
   
   return 0;
 }
